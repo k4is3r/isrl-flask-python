4f80af0cb0f9b3e5b3b41f5838f99333ad820992
Eduardo Imery <ejwimery@gmail.com>
2020-01-09 15:42:50 -0600

    setting up app to strt server

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import os
-from flask import Flask
+from flask import Flask, render_templates, requests, send_from_directory
+
+
+app = Flask(__name__)
+
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
