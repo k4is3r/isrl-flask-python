72e645314404ade44157f08a617052884ab3d135
Eduardo Imery <ejwimery@gmail.com>
2020-01-09 17:06:58 -0600

    getting data

--- app.py
+++ app.py
@@ -10,14 +10,20 @@ APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 @app.route('/', methods=['GET','POST'])
 def index():
     errors = []
-    
+    result = []
     if request.method == 'POST':
         try:
-            salario = request.form['salario']
+            salario = float(request.form['salario'])
+            tipoafp = int(request.form['tipoAfp'])
             print(f'Este es el salario ingresado {salario}')
+            print(f'El tipo de afp es {tipoafp}')
+            print(type(salario))
+            print(type(tipoafp))
+            result.append(salario)
+            result.append(tipoafp)
         except:
             errors.append("No se pudo obtener el salario")
-    return render_template('index.html')
+    return render_template('index.html',result=result)
 
 if __name__ == '__main__':
     app.run(debug = True)
