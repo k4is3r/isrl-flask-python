223f9186921136ade52cd973e3880f0f8526a2ca
Eduardo Imery <ejwimery@gmail.com>
2020-01-09 15:50:50 -0600

    change index page and setting up server

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, render_templates, requests, send_from_directory
+from flask import Flask, render_template, request, send_from_directory
 
 
 app = Flask(__name__)
