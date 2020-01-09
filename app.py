ea3e9bb6e82a4d8cdf05233142920e28bba1de4a
Eduardo Imery <ejwimery@gmail.com>
2020-01-09 16:52:53 -0600

    getting data form the html

--- app.py
+++ app.py
@@ -7,8 +7,16 @@ app = Flask(__name__)
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
+    errors = []
+    
+    if request.method == 'POST':
+    try:
+        salario = request.form['salario']
+        print(f'Este es el salario ingresado {salario}')
+    except:
+        errors.append("No se pudo obtener el salario")
     return render_template('index.html')
 
 if __name__ == '__main__':
