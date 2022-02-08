c8a6dce83fbb1ec493739726348e1b5cb8736349
Eduardo Winterdaal <ejwimery@gmail.com>
2022-02-08 16:45:27 -0600

    fixing problems in fastapi change

--- app.py
+++ app.py
@@ -1,38 +1,46 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
-from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from typing import Optional
 import isrlCalc as isrlc
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
+
 @app.get("/check-status")
 def check_healt():
-    return {"Status":"Active"}
+    return {"Status": "Active"}
+
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse(
+                        "index.html",
+                        {"request": request})
+
 
 @app.post("/handle-salary", response_class=HTMLResponse)
-def handle_salary(request: Request):
+async def handle_salary(request: Request):
     errors = []
     resp = []
     result = []
-    if request.method == 'POST':
+    form_send = await request.form()
+    if request.method == "POST":
         try:
-            month_salary = float(request.form['salario'])
-            afp_type = int(request.form['tipoAfp'])
+            month_salary = float(form_send['salario'])
+            afp_type = int(form_send['tipoAfp'])
+            print(month_salary, afp_type)
             resp.append(month_salary)
             resp.append(afp_type)
-        except :
+        except request.NotSalaryFound:
             errors.append("No se pudo obtener el salario")
 
     if resp:
         result = isrlc.islr(resp[0], resp[1])
         result.append(resp[0])
 
-    return templates.TemplateResponse("index.html", {"request":request, "result":result})
+    return templates.TemplateResponse(
+                        "index.html",
+                        {"request": request,
+                         "result": result})
